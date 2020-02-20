ef80a73f0a9bcbdd4622ab027b318ceb31026bad
benji127 <eneapaja27@gmail.com>
2020-02-20 18:10:39 +0100

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   imports: [
@@ -32,7 +33,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    AlertComponent
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
